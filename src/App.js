7b4a25a56bba8358dd87c58a78291a01be931a8f
victor <victor.magalhaesp@gmail.com>
2020-05-06 12:57:23 -0300

    Adicionando projeto simples de react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
+
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+import Welcome from './Welcome';
+import Secured from './Secured';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <ul>
+          <li><Link to="/">public component</Link></li>
+          <li><Link to="/secured">secured component</Link></li>
+        </ul>
+        <Route exact path="/" component={Welcome} />
+        <Route path="/secured" component={Secured} />
+      </div>
+    </BrowserRouter>
   );
 }
 
